354b17d2bb6c99a9dc233cee7f81fbec46b2409a
cba <cba@jupiter.seb>
2022-06-14 09:52:18 +0200

    prepare tree for import

--- Benchmarks.cmd
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-dotnet run -p .\tests\Benchmark\ -c Release -f netcoreapp2.2 --runtimes net472 netcoreapp2.2 -m
--- Toys/Utils.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Pipelines.Sockets.Unofficial
-{
-    public static class Utils
-    {
-        public static TextWriter DebugLog = TextWriter.Null;
-    }
-}
